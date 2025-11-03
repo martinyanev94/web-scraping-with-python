68fd83b166a221bc6627a5f8cb2f788ae5883e89
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-11-03 15:13:08 -0500

    Add Lesson_21_Effective_Data_Collection_Strategies/part_2_http_request_with_headers.py

---
+++ Lesson_21_Effective_Data_Collection_Strategies/part_2_http_request_with_headers.py
@@ -0,0 +1,5 @@
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+}
+
+response = requests.get(url, headers=headers)
