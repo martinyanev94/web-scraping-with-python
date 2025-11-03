b87151b1e8396564efa7016ef16047a3782da3d7
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-11-03 15:14:32 -0500

    Add Lesson_30_Understanding_Website_Technology_Implications/part_4_random_user_agent_request.py

---
+++ Lesson_30_Understanding_Website_Technology_Implications/part_4_random_user_agent_request.py
@@ -0,0 +1,16 @@
+import requests
+import random
+
+user_agents = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Firefox/89.0.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15"
+]
+
+headers = {
+    "User-Agent": random.choice(user_agents)
+}
+
+response = requests.get('https://www.example.com', headers=headers)
+
+print(response.text)
